7928d539ac592526b4f90ba04346bbd196028774
godckgo <maximum_2@icloud.com>
2023-07-01 12:42:05 +0900

    230701 udemy Spring framework 6 & Spring Boot 3 강의 first commit

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		// 1: Object creation
+		var game 		= new PacmanGame();	
+		// 2: Object creation + Wiring of Dependencies
+		var gameRunner 	= new GameRunner(game);	
+		
+		gameRunner.run();
+	}
+
+}
